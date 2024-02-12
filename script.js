91a804ae33bf5c542deaa3518e47b658565cf71d
SlimTracy <90283597+SlimTracy@users.noreply.github.com>
2024-02-11 23:25:09 -0600

    fixed the javascript for the search bar

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-// Your JavaScript code goes here
+document.getElementById("searchForm").addEventListener("submit", function(event) {
+    event.preventDefault();
+    
+    var searchTerm = document.getElementById("searchInput").value;
+    var encodedSearchTerm = encodeURIComponent(searchTerm);
+    
+    var searchUrl = "https://www.facebook.com/marketplace/category/search?deliveryMethod=local_pick_up&sortBy=creation_time_descend&DaysSinceListed=1&delivery%20Method=local_pick_up&query=" + encodedSearchTerm + " ";
+    
+    // Redirect to the generated search URL
+    window.location.href = searchUrl;
+  });
+  
